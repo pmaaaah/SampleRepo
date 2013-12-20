5afd666a2c2856279ad092a48201cb36a1275807
Patrick <patrick@patrickmarr.com>
2013-12-20 00:07:20 -0500

    first round of tutorial through 5.7

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,4 +1,6 @@
 Blog::Application.routes.draw do
+  get "welcome/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -12,7 +14,8 @@ Blog::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  resources :posts
+ 
   # Sample resource route with options:
   #   resources :products do
   #     member do
@@ -48,7 +51,7 @@ Blog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
